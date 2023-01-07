824e777ec4776d4415fd5868e8c34f0bde00b8a9
abdulasad04 <119759126+abdulasad04@users.noreply.github.com>
2023-01-07 12:50:18 -0500

    Update Temperature_Distribution_Code.py

--- Temperature_Distribution_Code.py
+++ Temperature_Distribution_Code.py
@@ -14,7 +14,7 @@ t = 6
 length = 0.563
 # radius of the tip of the nose cone measured in meters. The thickness of the nose cone is 2cm therefore that should be
 # the min tip radius
-radius_nc_tip = 0.02
+radius_nc_tip = 0.002
 # Material/thermal conductivity in W/m K
 conduc = 0.04
 # Specific heat capacity in J/Kg * K
@@ -76,6 +76,26 @@ def heat_solver(kappa, emiss, L, T_init, heat_flux, conduc):
         temp_store.append(copy.deepcopy(temperature))
     return pos, t, temp_store
 
+def get_flux_heat_with_HWC(radius_nc_tip, v, temperature, pos):
+    '''Returns the heat flux with the correction factor included in W/m^2 given radius of nose cone at tip in m, temperature of the nose cone in degrees celcius and position along the nose cone in meters and velocity v in m/s'''
+    N = 0.5
+    M = 3
+    # air density at 20 degrees celcius in Kg/m^3
+    air_density = 1.2
+    # Formula: 1.83*10^-8/r_tip^0.5
+    C = 1.83*10**-8/(radius_nc_tip**0.5)
+
+    #Calculating hw and hinf
+    #Specific heat capcity of ait in J/kg K
+    specific_heat_air = 1000
+    #Calculating integral of Cp times temp
+    integ=0
+    for idx in range(len(temperature)-1):
+        integ += specific_heat_air*(temperature[idx]+temperature[idx+1])/2 *(pos[idx+1]-pos[idx])
+    # correction factor
+    gw = integ/(integ+0.5*v**2)
+    #Equation: C*rho^N*c^M*(1-hw/hinf)
+    return C * air_density**N*v**M*(1-gw)
 
 def thermal_tool(radius_nc_tip, length, T_init, diffusivity, emiss, conduc):
     '''Takes in the radius of the tip of the nose cone, the length, the initial temperature, the diffusivity, the emissivity, and the conductivity of the nose cone and returns the temperature profile'''
@@ -90,19 +110,41 @@ def thermal_tool(radius_nc_tip, length, T_init, diffusivity, emiss, conduc):
         # Gets the time to plot heat flux versus time
         time.append(float(line[0]))
         line = simulation_values.readline()
+
+    #Calculating initial temperature estimate
+    x, t, Tstr = heat_solver(diffusivity, emiss, length, T_init, heat_flux, conduc)
+
+
+    # Iterative solver
+    #Change this based on how accurate you want the temperature distribution to be (Larger numbers are more accurate but take more time)
+    n = 3
+    for i in range(n):
+        #Recalculating heat flux with correction factor based on temperature
+        simulation_values = open("openrocket_values.txt")
+        line = simulation_values.readline()
+        heat_flux = []
+        idx = 0
+
+        while line:
+            line = line.split()
+            heat_flux.append(get_flux_heat_with_HWC(radius_nc_tip, float(line[2]), Tstr[0][idx], x))
+            line = simulation_values.readline()
+        #Recalculating heat flux based on new temperature
+        x, t, Tstr = heat_solver(diffusivity, emiss, length, T_init, heat_flux, conduc)
+
     plt.plot(time, heat_flux)
-    #plotting heat flux with respect to time
+    # plotting heat flux with respect to time
     plt.title("heat flux with respect to time")
     plt.xlabel("Time (s)")
     plt.ylabel("heat flux (W/m^2)")
     plt.show()
-    #plotting heat distribution across the length of the nose cone
-    x, t, Tstr = heat_solver(diffusivity, emiss, length, T_init, heat_flux, conduc)
+    # plotting heat distribution across the length of the nose cone
     plt.plot(x, np.transpose(Tstr[-1]))
     plt.title("heat distibution across nose cone")
     plt.xlabel("Length (m)")
     plt.ylabel("Temperature (Celcius)")
     plt.show()
     print("Max Temp: ", Tstr[-1][0][0], "Celcius")
-    
+
+
 thermal_tool(radius_nc_tip, length, T_init, conduc/(heatcapac*rho), emissivity, conduc)
